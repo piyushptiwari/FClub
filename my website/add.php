d39fe6e5e6b930343ac7514d40ac45e4431ab029
Piyush tiwari <piyushptiwari@gmail.com>
2019-11-08 00:31:10 +0530

    Add files via upload

---
+++ my website/add.php
@@ -0,0 +1,9 @@
+<html
+<body>
+<form action="add.php" method="GET">
+<input type="number" size='5' name='num1'>
+<input type="number" size='5' name='num2'>
+<input type="submit" value="add">
+</form>
+</body>
+</html>
